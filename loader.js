281a7f9673774c1b080b0c53f0568980a002b719
s36127-pja <s36127@pjwstk.edu.pl>
2026-06-10 22:27:24 +0200

    FIXED and omg
    life is not bonita rn

--- loader.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", ()=>{
-    const loader = document.querySelector(".lScreen");
-
-    loader.classList.add("lHidden");
-
-    loader.addEventListener("transitioned", () =>{
-        document.body.removeChild("lScreen");
-    })
-})
--- testing/bg_fade.js
+++
@@ -1,9 +0,0 @@
-var element = document.querySelector('#onloading');
-
-function show_PageBody(){
-    setTimeout(function(){update_Wrapper(); },1000);
-    function update_Wrapper(){
-        document.getElementById('BodyWrap').style.display='block';
-        document.getElementById('BodyWrap').style.zIndex='5';
-    }
-}
